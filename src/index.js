6c1716065758989caee5e127f230416c01b9f3f0
Samuel Omoding <samuel.omoding@student.moringaschool.com>
2024-01-02 12:17:21 +0300

    Assignment attempted

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementById("create-task-form");
+  const taskList = document.getElementById("tasks");
+
+  form.addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    const taskDescription = document.getElementById("new-task-description").value;
+    addTaskToList(taskDescription);
+
+    //After adding the task, we clear the input field by resetting
+    form.requestFullscreen();
+});
+
+function addTaskToList(taskDescription)  {
+  const taskItem = document.createElement("li");
+  taskItem.textContent = taskDescription;
+  taskList.appendChild(taskItem);
+
+}
 });
